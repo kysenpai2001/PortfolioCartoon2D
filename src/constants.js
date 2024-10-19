0c89f1e2d452cb0f67a035a6a0433a7073b70ff0
kysenpai2001 <manhtony2001.q@gmail.com>
2024-10-19 08:53:59 +0700

    finish

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Đây là máy tính của em. Hiện nay em chủ yếu làm việc với JavaScript/ReactJS.
+    Em đã thực hiện một vài đồ án bằng ngôn ngữ đó. Em cũng thích C# và Python. Dù sao bất kể ngôn ngữ, em chỉ thích lập trình.
+    Đây là của em <a href="https://github.com/kysenpai2001" target="_blank">Github</a>!`,
+  "cs-degree": `Đây là bằng đại học của em. Em treo nó lên tường vì em tự hào về nó. Đó là một bằng cấp tuy lý thuyết nhiều nhưng em nghĩ nó đã cho em một nền tảng tốt.`,
+  "sofa-table": `Đó là ghế sofa của em. Em thích thư giãn ở đây và xem YouTube. 
+  Em cũng thích xem youtube của một số phim như là Marvel, DC, Netflix,...`,
+  tv: `Đó là tivi của em. Gần đây em hay xem các YouTuber công nghệ như :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `Đây là nơi em ngủ. Những ý tưởng tuyệt vời xuất hiện khi em đang nằm trên giường. Khi nảy ra một ý tưởng, em thường phải viết nó ra, nếu không em sẽ không thể ngủ được vì năng lượng tinh thần của em bị tiêu hao bởi nó.`,
+  resume: `Đây là bàn làm việc của em và trên đó là CV của em. <a href="https://github.com/kysenpai2001/resume/blob/main/MyCV.pdf" target="_blank">Nhấp vào đây</a>
+  Liên hệ qua manhquynh2001.it@gmail.com nếu anh chị có bất kỳ cơ hội việc làm hay ho nào!`,
+  projects: `Thông tin về portfolio này : Nó được tạo bằng thư viện Kaboom.js, một thư viện để tạo trò chơi bằng JavaScript.
+  Văn bản được hiển thị bằng HTML/CSS. Vì vậy, hộp văn bản đang đọc không được hiển thị trong khung vẽ. Tìm hiểu thêm về cách sử dụng
+  Kaboom.js bằng cách xem một số hướng dẫn của qua kênh này <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `Có rất nhiều sách lập trình trên kệ của em. Thậm chí còn có một số cuốn bằng tiếng Anh (tôi cũng có thể đọc hiểu tiếng Anh).
+  Có lẽ em chỉ đọc một trong số đó. Ngoài ra còn mấy cuốn truyện tranh marvel và DC`,
+  exit: `Nếu anh chị muốn thoát khỏi portfolio của em, chỉ cần đóng tab.`,
+};
